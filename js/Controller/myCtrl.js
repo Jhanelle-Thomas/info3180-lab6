5b3c875c141f9f2adb6ad8e01358949fbec6ee0d
Jhanelle-Thomas <jhanelle.thomas@yahoo.com>
2017-03-19 19:37:27 -0500

    error handling

--- js/Controller/myCtrl.js
+++ js/Controller/myCtrl.js
@@ -2,10 +2,17 @@ app.controller("myCtrl", function($scope) {
 	$scope.products = ["Milk", "Bread", "Cheese"];
 	
 	$scope.addItem = function() {
-	    $scope.products.push($scope.addMe);   
+	    $scope.errortext = "";
+	    if(!$scope.addMe) { return; }
+	    if($scope.products.indexOf($scope.addMe) == -1) {
+	        $scope.products.push($scope.addMe);
+	    } else {
+	        $scope.errortext = "The item is already in your shopping list.";
+	    }   
 	}
 	
 	$scope.removeItem = function(index) {
+	    $scope.errortext = "";
 	    $scope.products.splice(x,1);
 	}
 });
